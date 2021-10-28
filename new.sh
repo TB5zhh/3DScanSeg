a4f4d002b068856cb597f72929723d1d3c7088d6
tb5zhh <tianbeiwen88@gmail.com>
2021-10-29 00:42:24 +0800

    modify absolute path in scripts

--- new.sh
+++ new.sh
@@ -4,15 +4,15 @@ python -u new.py \
     --seed 42 \
     --train_dataset ScannetVoxelization2cmDataset \
     --val_dataset ScannetVoxelization2cmtestDataset \
-    --scannet_path /home/cloudroot/data/scannet_processed/$POINTS/train \
-    --scannet_test_path /home/cloudroot/data/scannet_processed/full/train \
+    --scannet_path /home/aidrive/tb5zhh/data/$POINTS/train \
+    --scannet_test_path /home/aidrive/tb5zhh/data/full/train \
     --model Res16UNet34CUNC \
     --weights checkpoints/pretrain_20000.pth \
     --checkpoint_dir checkpoints \
     --num_workers 4 \
     --validate_step 50 \
     --optim_step 1 \
-    --train_batch_size 8   \
+    --train_batch_size 16   \
     --val_batch_size 24  \
     --lr 0.1 \
     --save_epoch 5 \
@@ -20,9 +20,9 @@ python -u new.py \
     --run_name finetune_b_$POINTS \
     --scheduler PolyLR \
     --do_train \
+    --resume /home/aidrive/tb5zhh/3DScanSeg/checkpoints/finetune_b_${POINTS}_latest.pth \
     --wandb True
     
-    # --resume /home/cloudroot/3DScanSeg/checkpoints/finetune_${POINTS}_latest.pth \
 
 # finetune_a: move dropout layer before the linear layer
 # finetune_b: smaller batch size
--- unc_inference.sh
+++ unc_inference.sh
@@ -7,15 +7,14 @@ python -u new.py \
     --log_dir log \
     --seed 42 \
     --unc_dataset ScannetVoxelization2cmtestDataset \
-    --scannet_path /home/cloudroot/data/scannet_processed/$POINTS/train \
-    --scannet_test_path /home/cloudroot/data/scannet_processed/full/train \
+    --scannet_path ~/data/$POINTS/train \
+    --scannet_test_path ~/data/full/train \
     --model Res16UNet34CUNC \
-    --weights /mnt/air-02/luoly/tbw/log/checkpoint_NoneRes16UNet34C_20000.pth \
     --checkpoint_dir checkpoints \
-    --num_workers 0 \
+    --num_workers 2 \
     --test_batch_size 24 \
     --run_name unc_inference_$POINTS \
-    --resume /home/cloudroot/3DScanSeg/checkpoints/finetune_b_${POINTS}_best_bak.pth \
+    --resume ~/3DScanSeg/checkpoints/finetune_b_${POINTS}_best.pth \
     --unc_result_dir $UNC_RESULT_DIR \
     --unc_round 50 \
     --do_unc_inference
